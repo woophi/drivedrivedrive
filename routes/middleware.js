ae43c26c9cbf1c513202e6cc1d5a63f06699fe41
konst <konstantin.mikheev@icord.cz>
2018-03-04 16:22:30 +0100

    create registration 1 step

--- routes/middleware.js
+++ routes/middleware.js
@@ -50,7 +50,7 @@ exports.flashMessages = function (req, res, next) {
 exports.requireUser = function (req, res, next) {
 	if (!req.user) {
 		req.flash('error', 'Please sign in to access this page.');
-		res.redirect('/keystone/signin');
+		res.redirect('/admin/signin');
 	} else {
 		next();
 	}
