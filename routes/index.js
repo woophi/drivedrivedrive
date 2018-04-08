30ce13ea4e082cd444dc88c32b5a0663cdbfb75e
konst <konstantin.mikheev@icord.cz>
2018-04-08 15:27:49 +0200

    reset password

--- routes/index.js
+++ routes/index.js
@@ -45,6 +45,7 @@ exports = module.exports = function (app) {
 	app.all('/signin', routes.views.session.signin);
 	app.get('/signout', routes.views.session.signout);
 	app.all('/forgot-password', routes.views.session['forgot-password']);
+	app.all('/reset-password/:key', routes.views.session['reset-password']);
 
 	// User
 	app.all('/me*', middleware.requireUser);
