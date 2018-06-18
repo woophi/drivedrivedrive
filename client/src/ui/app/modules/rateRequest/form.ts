6ba95f28eced98be8e04f77976054c9b3d9b5d08
Konstantin <konstantin.mikheev@icord.cz>
2018-06-18 14:20:58 +0200

    fixed rating form

--- client/src/ui/app/modules/rateRequest/form.ts
+++ client/src/ui/app/modules/rateRequest/form.ts
@@ -1,41 +1,59 @@
-import { RateRequest } from 'core/models/api';
-import { change, FormErrors, FormSubmitHandler, reset, SubmissionError } from 'redux-form';
-import { getRequestToRate, rateRequest } from './operations';
+import { RateRequest } from "core/models/api";
+import {
+  FormErrors,
+  FormSubmitHandler,
+  reset,
+  SubmissionError
+} from "redux-form";
+import { getRequestToRate, rateRequest } from "./operations";
 
 type SharedProps = {
   requestId: string;
   query: number;
-}
+};
 
-export const validateRR = (values: Partial<RateRequest>): FormErrors<RateRequest> => {
+export const validateRR = (
+  values: Partial<RateRequest>
+): FormErrors<RateRequest> => {
   const errors = {} as FormErrors<RateRequest>;
   if (!values.ratingCar) {
-    errors.ratingCar = 'Пожалуйста, поставьте оценку';
+    errors.ratingCar = "Пожалуйста, поставьте оценку";
   }
   if (!values.ratingDriver) {
-    errors.ratingDriver = 'Пожалуйста, поставьте оценку';
+    errors.ratingDriver = "Пожалуйста, поставьте оценку";
   }
   if (!values.ratingTrip) {
-    errors.ratingTrip = 'Пожалуйста, поставьте оценку';
+    errors.ratingTrip = "Пожалуйста, поставьте оценку";
   }
 
-  if (values && (values.ratingTrip <= 4 || values.ratingDriver <= 4 || values.ratingCar <= 4)) {
-    errors.ratingComment = 'Пожалуйста, напишите, что Вам не понравилось';
+  if (
+    values &&
+    (values.ratingTrip <= 4 ||
+      values.ratingDriver <= 4 ||
+      values.ratingCar <= 4) &&
+    !values.ratingComment
+  ) {
+    errors.ratingComment = "Пожалуйста, напишите, что Вам не понравилось";
   }
 
   return errors;
 };
 
-export const submitRR: FormSubmitHandler<RateRequest> = async (values: RateRequest, dispatch, props: SharedProps) => {
+export const submitRR: FormSubmitHandler<RateRequest> = async (
+  values: RateRequest,
+  dispatch,
+  props: SharedProps
+) => {
   try {
     const payload: RateRequest = {
       requestId: props.requestId,
       ...values
     };
-    await rateRequest(payload)
-      .then(() => getRequestToRate(props.requestId, props.query));
-    dispatch(reset('assignRequest'));
+    await rateRequest(payload).then(() =>
+      getRequestToRate(props.requestId, props.query)
+    );
+    dispatch(reset("assignRequest"));
   } catch (e) {
-    throw new SubmissionError({  _error: e.error.message || e.error });
+    throw new SubmissionError({ _error: e.error.message || e.error });
   }
 };
