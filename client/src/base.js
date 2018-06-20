ec8a5240941c47e55b7e262b530bda264ea76b80
konst <konstantin.mikheev@icord.cz>
2018-06-20 20:50:26 +0200

    revert base

--- client/src/base.js
+++ client/src/base.js
@@ -1,12 +1,17 @@
 /**
  * Only add libraries when they are used as whole.
  */
-// require('moment');
+
+require('axios');
+require('fela-plugin-embedded');
+require('moment');
 require('react');
 require('react-dom');
-// require('react-fela');
-// require('react-redux');
+require('react-fela');
+require('react-virtualized');
+require('react-redux');
 require('redux-observable');
 require('tslib');
+require('react-custom-scrollbars');
 require('react-loadable');
 require('react-visibility-sensor');
