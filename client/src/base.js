3ef413951fa47e02fbbf37ba3c69d21c6949ea05
Konstantin <konstantin.mikheev@icord.cz>
2018-06-20 10:09:48 +0200

    reduce size of lib base

--- client/src/base.js
+++ client/src/base.js
@@ -1,11 +1,11 @@
 /**
  * Only add libraries when they are used as whole.
  */
-require('moment');
+// require('moment');
 require('react');
 require('react-dom');
-require('react-fela');
-require('react-redux');
+// require('react-fela');
+// require('react-redux');
 require('redux-observable');
 require('tslib');
 require('react-loadable');
