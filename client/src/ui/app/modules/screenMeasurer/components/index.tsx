779844a8fea62ce32ad9c8a1b0654fcfd0cf0db0
konst <konstantin.mikheev@icord.cz>
2018-07-21 14:17:45 +0200

    upgrade packages

--- client/src/ui/app/modules/screenMeasurer/components/index.tsx
+++ client/src/ui/app/modules/screenMeasurer/components/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { AppDispatch } from 'core/models/app';
-import { connect as reduxConnect, Dispatch } from 'react-redux';
-import { AutoSizer, Dimensions } from 'react-virtualized';
+import { connect as reduxConnect, DispatchProp } from 'react-redux';
+import { AutoSizer } from 'react-virtualized';
 import { bindActionCreators } from 'redux';
 import * as actions from '../actions';
 
-export default reduxConnect(null, (dispatch: Dispatch<AppDispatch>) =>
-  bindActionCreators({ ...actions }, dispatch)
+export default reduxConnect(null, (dispatch: DispatchProp<AppDispatch>) =>
+  bindActionCreators({ ...actions }, dispatch as any)
 )(({ setDimestions }) => {
   return (
     <AutoSizer>
