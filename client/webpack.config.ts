a70ecbe38d2ff763bf99f8a0161e44743599f1fd
konst <konstantin.mikheev@icord.cz>
2018-09-16 11:38:36 +0200

    revert

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -140,28 +140,7 @@ const config = {
       loaders: ['css-loader?sourceMap&minimize&-autoprefixer&safe'],
       threadPool
     })
-  ],
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: Infinity,
-      minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module: any) {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-            // npm package names are URL-safe, but some servers don't like @ symbols
-            return `npm.${packageName.replace('@', '')}`;
-          },
-        },
-      },
-    },
-  },
+  ]
 };
 
 export default config;
